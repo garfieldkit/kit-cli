c6c86629c6426543e6c51cf8fa54ad498cd50db1
qetuozm <yinjianihao@qq.com>
2022-10-06 23:55:51 +0800

    chore: upload oss util3

--- replace/lib/index.js
+++ replace/lib/index.js
@@ -95,3 +95,49 @@ async function lookAtImages() {
     })
   })
 }
+
+
+async function uploadAllImagesToOSS(imageDirs, params) {
+  const imageObj = {}
+  const results = []
+  const oss = require('ali-oss')({
+    accessKeyId: params.ossAccessKey,
+    accessKeySecret: params.ossSecretKey,
+    bucket: params.bucket,
+    region: params.region,
+  })
+  for (const dir of imageDirs) {
+    const result = await uploadImageToOSS(dir, oss)
+    results.push(...result)
+  }
+  results.map(item => imageObj[item.key] = item.url)
+  return imageObj
+}
+
+async function uploadImageToOSS(imageDir, oss) {
+  return new Promise((resolve, reject) => {
+    glob(`${imageDir}/**`, {
+      nodir: true,
+    }, async function(err, files) {
+      if (err) {
+        reject(err)
+      } else {
+        const o = {}
+        for (const file of files) {
+          const localPath = path.resolve(file)
+          log.info('开始上传[' + localPath + ']...')
+          const ossRes = await oss.put(file, localPath)
+          if (ossRes && ossRes.res.status === 200) {
+            o[file] = ossRes.url
+          }
+        }
+        resolve(files.map(file => ({
+          key: file,
+          url: o[file],
+        })))
+      }
+    })
+  })
+}
+
+module.exports = replace
